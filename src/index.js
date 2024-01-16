4ebc5e5403de2767cbd147ddeaa23dd96f274b9a
Lucas Pizol <lucaspizolfe@gmail.com>
2024-01-16 01:30:40 -0300

    removendo cache

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const server = require("http").createServer(app);
 const qrcode = require("qrcode")
 const { Client, LocalAuth } = require("whatsapp-web.js")
 
+let isActive = false
+
 const client = new Client({
   authStrategy: new LocalAuth()
 })
@@ -25,22 +27,25 @@ server.listen(3001, () => {
 })
 
 io.on("connection", (socket) => {
+
   socket.emit("qr", "https://i.gifer.com/origin/34/34338d26023e5515f6cc8969aa027bca.gif");
 
   client.on("qr", (qr) => {
+    console.log(qr)
     qrcode.toDataURL(qr, (err, url) => {
       socket.emit("qr", url);
     });
   })
-  
+
   client.on("ready", () => {
     socket.emit("qr", "https://static.vecteezy.com/system/resources/thumbnails/001/200/261/small/check.png");
   })
+  
   console.log("User connected: " + socket.id)
 })
 
 client.on("message", (message) => {
-  if (message.from === "14155238886@c.us" ) {
+  if (message.from === "14155238886@c.us") {
     client.sendMessage(message.from, "Fala tu")
   }
 })
